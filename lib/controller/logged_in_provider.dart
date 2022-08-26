2e83fc1430badf83e89ca2ab9fcfdb85fad04b5d
ishaque <ishaque.kannoth@gmail.com>
2022-08-26 20:22:22 +0530

    Working with stream builder

--- lib/controller/logged_in_provider.dart
+++ lib/controller/logged_in_provider.dart
@@ -1,8 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fire_base_first/model/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 
 class LoggedInProv extends ChangeNotifier {
+  Stream userDataStream(User user) async* {
+    var document =
+        FirebaseFirestore.instance.collection('users').doc(user.email).snapshots();
+    yield document;
+  }
+
+  // getUser() {
+  //   var collection = FirebaseFirestore.instance
+  //       .collection('users')
+  //       .get()
+  //       .then((QuerySnapshot snapshot) {
+  //     snapshot.docs.forEach((DocumentSnapshot element) {
+  //       print(element.data());
+  //     });
+  //   });
+  // }
+
   Future<UserModel> getUserDetails(String email) async {
     var doc =
         await FirebaseFirestore.instance.collection('users').doc(email).get();
--- lib/view/logged_in_page.dart
+++ lib/view/logged_in_page.dart
@@ -1,3 +1,6 @@
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fire_base_first/controller/logged_in_provider.dart';
 import 'package:fire_base_first/model/user_model.dart';
 import 'package:fire_base_first/view/login_screen.dart';
@@ -8,15 +11,17 @@ import 'package:provider/provider.dart';
 class LoggedIn extends StatelessWidget {
   LoggedIn({Key? key}) : super(key: key);
   final user = FirebaseAuth.instance.currentUser;
-
   @override
   Widget build(BuildContext context) {
+   
     return SafeArea(
       child: (Scaffold(
-          body: FutureBuilder(
-        future: Provider.of<LoggedInProv>(context, listen: false)
-            .getUserDetails(user!.email.toString()),
-        builder: (context, AsyncSnapshot<UserModel> snapshot) {
+          body:StreamBuilder(
+        stream:FirebaseFirestore.instance.collection('users').doc(user!.email).snapshots(),
+        //  Provider.of<LoggedInProv>(context, listen: false)
+        //     .getUserDetails(user!.email.toString()),
+        builder: (context, AsyncSnapshot snapshot) {
+          
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
@@ -25,11 +30,9 @@ class LoggedIn extends StatelessWidget {
             return Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Center(child: Text("Logged in as ${snapshot.data!.name}")),
-                Text("Last name ${snapshot.data!.lastName}"),
-                Text("Profession ${snapshot.data!.profession}"),
-                
-                
+                Center(child: Text("Logged in as ${UserModel.fromMap(snapshot.data).name}")),
+                Text("Last name ${UserModel.fromMap(snapshot.data).lastName}"),
+                Text("Profession ${UserModel.fromMap(snapshot.data).profession}"),
                 ElevatedButton(
                     onPressed: () async {
                       await FirebaseAuth.instance.signOut().then((value) =>
