8f66d68c692d2c9a68b51a55af7a059f3c13e3fd
ishaque <ishaque.kannoth@gmail.com>
2022-08-26 15:36:31 +0530

    Working future builder

--- lib/controller/logged_in_provider.dart
+++ lib/controller/logged_in_provider.dart
@@ -1,13 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fire_base_first/model/user_model.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 
 class LoggedInProv extends ChangeNotifier {
-
-  getUserDetails(String email) async {
+  Future<UserModel> getUserDetails(String email) async {
     var doc =
         await FirebaseFirestore.instance.collection('users').doc(email).get();
-    print(UserModel.fromMap(doc).toMapped().toString());
+    return (UserModel.fromMap(doc));
   }
 }
--- lib/view/logged_in_page.dart
+++ lib/view/logged_in_page.dart
@@ -1,5 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fire_base_first/controller/logged_in_provider.dart';
+import 'package:fire_base_first/model/user_model.dart';
 import 'package:fire_base_first/view/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -12,16 +12,24 @@ class LoggedIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: (Scaffold(body: FutureBuilder(
-        future: Provider.of<LoggedInProv>(context).getUserDetails(user!.email!),
-        builder: (context, snapshot) {
+      child: (Scaffold(
+          body: FutureBuilder(
+        future: Provider.of<LoggedInProv>(context, listen: false)
+            .getUserDetails(user!.email.toString()),
+        builder: (context, AsyncSnapshot<UserModel> snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Errr occured'));
           } else {
             return Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Center(child: Text("Logged in as ${user!.email}")),
+                Center(child: Text("Logged in as ${snapshot.data!.name}")),
+                Text("Last name ${snapshot.data!.lastName}"),
+                Text("Profession ${snapshot.data!.profession}"),
+                
+                
                 ElevatedButton(
                     onPressed: () async {
                       await FirebaseAuth.instance.signOut().then((value) =>
@@ -38,5 +46,4 @@ class LoggedIn extends StatelessWidget {
       ))),
     );
   }
-
 }
